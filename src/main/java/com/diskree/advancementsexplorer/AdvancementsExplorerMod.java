3783fc333e7cfade0f62aece1f296763f7d7aa9e
diskree <diskreee@gmail.com>
2025-01-15 02:18:51 +0400

    Initial commit

---
+++ src/main/java/com/diskree/advancementsexplorer/AdvancementsExplorerMod.java
@@ -0,0 +1,10 @@
+package com.diskree.advancementsexplorer;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AdvancementsExplorerMod implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
